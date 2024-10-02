6655339ed7c794df54053e58aead1d61fafd0f6d
tapshyamangal567 <mangaltapasya@gmail.com>
2024-10-02 11:23:41 +0530

    Add files via upload

---
+++ day 5 - water filling.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+	
+	int B1,B2,B3;
+	
+	printf("enter the status of bottle (0 = empty,1 = filled: )");
+	scanf("%d %d %d", &B1,&B2,&B3);
+	
+	if(B1,B2,B3 == 0) {
+		
+		printf("water filling time");
+	}  else {
+		printf("not now");
+	}
+		
+	
+}
