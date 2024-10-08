8dfcf9b6a6b50ffbdd0427207769ebfcf2440854
tapshyamangal567 <mangaltapasya@gmail.com>
2024-10-08 20:16:43 +0530

    Add files via upload

---
+++ day 11 - c program to check a number is +,- or zero.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+ int main()
+ {
+ 	int num;
+ 	
+ 	printf("enter any number: ");
+ 	scanf("%d",&num);
+ 	
+ 	if(num>0)
+ 	{
+ 		printf("number is positive");
+	 }
+	 
+	 if(num<0)
+	 {
+	 	printf("number is negative");
+	 }
+	 
+	 if(num==0)
+	 {
+	 	printf("number is zero");
+	 }
+ }
